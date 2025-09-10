04a854543500fa37deb6b5188e9a758d84b6c2d4
Andrey <vstup230921@gmail.com>
2025-09-10 21:15:19 +0300

    fix

--- car_sales/main/urls.py
+++ car_sales/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+app_name='main'
 urlpatterns = [
     path('',views.main_view,name='main'),
-    path("get-models/<slug:slug>/", views.get_models, name="get_models")
+    path("get-models/<slug:slug>/", views.get_models, name="get_models"),
+    path("search/", views.search, name="search"),
 ]
--- car_sales/main/views.py
+++ car_sales/main/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 from .constants import TYPES,REGIONS,COLORS
 
 def main_view(request):
+    print(request)
     brands=CarBrand.objects.all()
     context={'types':TYPES,'regions':REGIONS,'ranges':range(1900,datetime.date.today().year+1),'brands':brands,'colors':COLORS}
     return render(request,'main/index.html',context=context)
@@ -17,3 +18,17 @@ def get_models(request,slug):
         return JsonResponse(data, safe=False)
     except CarBrand.DoesNotExist:
         return JsonResponse([], safe=False)
+    
+def search(request):
+    print(request)
+    print("GET parameters:", request.GET)
+    
+    car_type = request.GET.get('type')
+    region = request.GET.get('region') 
+    brand = request.GET.get('brand')
+    model = request.GET.get('model')
+    year = request.GET.get('year')
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+    
+    print(f"Type: {car_type}, Region: {region}, Brand: {brand}, Model: {model}")
