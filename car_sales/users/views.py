0ebc167e9837501567547e0b5b50fe6c5682d33d
Andrey <vstup230921@gmail.com>
2025-10-05 22:54:45 +0300

    started creating profile view for another users and default chat view for watching all chats

--- car_sales/users/views.py
+++ car_sales/users/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm
+from .models import User
 
 @login_required
 def profile(request):
@@ -16,5 +17,10 @@ def profile(request):
     else:
         form=ProfileForm(instance=request.user)
     return render(request,'users/profile.html',{'form':form})
+    
+@login_required
+def user_profile(request,user):
+    user=User.objects.get(id=user.id)
+    return render(request,'users/user_profile.html')
     
         
