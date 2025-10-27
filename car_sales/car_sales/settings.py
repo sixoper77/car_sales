eec0e41c5cc721af2c3d097c1d15fb27a858c028
Andrey <vstup230921@gmail.com>
2025-10-27 12:14:01 +0200

    all_models graph

--- car_sales/car_sales/settings.py
+++ car_sales/car_sales/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     
+    'django_extensions',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
