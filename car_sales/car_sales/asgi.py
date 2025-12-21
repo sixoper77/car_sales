003c3eafacb6cdf07eb34045058ce1e3f7548aa5
Andrey <vstup230921@gmail.com>
2025-12-21 20:25:37 +0200

    Separated Gunicorn and Daphne workers to avoid blocking synchronous requests.

--- car_sales/car_sales/asgi.py
+++ car_sales/car_sales/asgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-from chat import routing
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -16,6 +15,8 @@ from channels.auth import AuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_sales.settings')
 
 django_asgi_app = get_asgi_application()
+
+from chat import routing
 
 application=ProtocolTypeRouter({
     'http':django_asgi_app,
